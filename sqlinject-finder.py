e5e06676ad58f1ef6957d39703958ea3b55eb32c
tdean87 <tdean87@gmail.com>
2010-11-20 05:56:53 +0000

    +cleaned up the output ++added the source IP and page to output +fixed the way POST requests were being handled

--- sqlinject-finder.py
+++ sqlinject-finder.py
@@ -28,43 +28,58 @@ def removeComments(val):
 	return val
 
 #checks for common sql injection tactics using all the variables from post or get data
-def checkSQL(vals):
-	for pair in vals:
-		var = pair[0] #the variable, i.e. in id=1, the var is id
-		val = pair[1] #the value, i.e. in id=1, the val is 1
-		val = val.decode('ascii') #not sure if this is really doing anything, but we need to deal with non ascii characters for analysis
-		val = urllib.unquote(val) #removes url encodings like %20 for space, etc
-		val = val.replace("+", " ") #sometimes in urls, instead of a space you can have a + . So, we want to remove those for analysis
-		#print val
-		
-		##### Look for obfuscation techniques ######
-		index = val.find("/*")
-		if index != -1:
-			print var + "=" + val
-			print "Might be attempting to obfuscate a SQL statement with a comment"
-			val = removeComments(val)
+def analyzeRequest(vals, sIP, page):
+	var = vals[0] #the variable, i.e. in id=1, the var is id
+	val = vals[1] #the value, i.e. in id=1, the val is 1
+	val = val.decode('ascii') #not sure if this is really doing anything, but we need to deal with non ascii characters for analysis
+	val = urllib.unquote(val) #removes url encodings like %20 for space, etc
+	val = val.replace("+", " ") #sometimes in urls, instead of a space you can have a + . So, we want to remove those for analysis
+	#print val
+	display = ["false", sIP, page, var, val]
+	##### Look for obfuscation techniques ######
+	index = val.find("/*")
+	if index != -1:
+		display[0] = True
+		display.append("Might be attempting to obfuscate a SQL statement with a comment")
+		val = removeComments(val)
 
-		##### Look for commenting out the end of a MSSQL statement ######
-		index = val.rfind("--")
+	##### Look for commenting out the end of a MSSQL statement ######
+	index = val.rfind("--")
+	if index != -1:
+		display[0] = True
+		display.append("Might be attempting to end a SQL statement by commenting out the remaining statement")
+	
+	##### Look for commenting out the end of a MySQL statement #####
+	index = val.rfind("#")
+	if index != -1:
+		display[0] = True
+		display.append("Might be attempting to end a SQL statement by commenting out the remaining statement")
+	
+	##### Look for common SQL syntax in the values of a param #####
+	sqlvals = ("cast(", "declare ", "select ", "union ", "varchar", "set(", "create ", " or ", " NULL,", " concat(")
+	for sql in sqlvals:
+		index = val.lower().find(sql)
 		if index != -1:
-			print var + "=" + val
-			print "Might be attempting to end a SQL statement by commenting out the remaining statement"
-		
-		##### Look for commenting out the end of a MySQL statement #####
-		index = val.rfind("#")
-		if index != -1:
-			print var + "=" + val
-			print "Might be attempting to end a SQL statement by commenting out the remaining statement"
-		
-		##### Look for common SQL syntax in the values of a param #####
-		sqlvals = ("cast(", "declare ", "select ", "union ", "varchar", "set(", "create ", " or ", " NULL,", " concat(")
-		for sql in sqlvals:
-			index = val.lower().find(sql)
-			if index != -1:
-				print var + "=" + val
-				print "Possible use of SQL syntax in variable"
-				break
+			display[0] = True
+			display.append("Possible use of SQL syntax in variable")
+			break
 
+	if display[0] == True:
+		print "Source : " + str(display[1])
+		print "Page   : " + str(display[2])
+		print "Value  : " + str(display[3]) + "=" + str(display[4])
+		for i in range(len(display)-5):
+			print "Reason : " + str(display[i+5])
+		print ""
+
+def octetIP(sIP):
+	ip = ""
+	for s in sIP:
+		ip = ip + str(ord(s)) + "."
+		
+	return ip[:-1]
+		
+		
 #reads the pcap file and parses out get and post requests for analysis
 def parsepcap(filename):
 	try:
@@ -78,7 +93,9 @@ def parsepcap(filename):
 	except:
 		print "Error reading file. Please make sure the file is a valid pcap file."
 		sys.exit()
-		
+	sIP=""
+	page=""
+	print ""
 	for ts, buf in pcap:
 		eth = dpkt.ethernet.Ethernet(buf)
 		ip = eth.data
@@ -90,20 +107,31 @@ def parsepcap(filename):
 				index = 1
 				try:
 					http = dpkt.http.Request(tcp.data)
+					url = http.uri
 					#deal with post data
 					if http.method == "POST":
 						getvals=http.body
+						index = url.find("?")
+						if index != -1:
+							page = url[:index]
+						else:
+							page = url
+						print "page = " + page
 					#deal with GET data
 					elif http.method == "GET":
-						url = http.uri
 						index = url.rfind("?")
 						if index != -1:
 							getvals = url[index+1:]
+							page = url[:index]
 					else:
 						index = -1
 				except:
 					data = tcp.data
-					index = data.count("\n") #need to look into this method a little more, basically, we want to get POST data out of other streams
+					index = str(data).find("POST")
+					if index == 0:
+						url = str(data).split(" ")
+						page = url[1] #POST is usually always the second value in the POST
+					index = str(data).count("\n") #need to look into this method a little more, basically, we want to get POST data out of other streams
 					if index == 0:
 						index = data.find("=")
 						if index != -1:
@@ -114,12 +142,11 @@ def parsepcap(filename):
 				#split up each variable and its cooresponding value
 				if index != -1:
 					getvals = getvals.split("&")
-					vals = []
 					for val in getvals:
 						i = val.find("=")
 						val = (val[:i], val[i+1:])
-						vals.append(val)
-					checkSQL(vals)
+						sIP = octetIP(ip.src)
+						analyzeRequest(val, sIP, page)
 				
 
 	f.close()				
